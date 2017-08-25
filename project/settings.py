dec7a5ae284de7c60cbd9ee174ac2deeea8a99c8
Joseph Harwood <Joey@Josephs-MacBook-Pro.local>
2017-08-24 22:51:06 -0400

    changing directory names

--- project/settings.py
+++ project/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'lolstats.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'lolstats.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
