6b1fe93176f51587461dc5a42f70314896c46467
Joseph Harwood <Joey@Josephs-MacBook-Pro.local>
2017-08-27 10:54:18 -0400

    Static table

--- ui/views.py
+++ ui/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    
+	testlist = []
+	testlist.append(["Test1", "Test2", "Test3"])
+	testlist.append(["Test4", "Test5", "Test6"])
+	testlist.append(["Test7", "Test8", "Test9"])
+	template = loader.get_template('ui/index.html')
+	context = { 'testlist': testlist }
+	return HttpResponse(template.render(context, request))
